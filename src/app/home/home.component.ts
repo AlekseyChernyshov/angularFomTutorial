8e8d58e1b86041baa45e19c3eaffcca49050229b
Alexey Chernyshov <ubiru77@gmail.com>
2024-01-23 21:36:06 +0400

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,6 +27,7 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  
   filteredLocationList: HousingLocation[] = [];
 
 constructor() {
